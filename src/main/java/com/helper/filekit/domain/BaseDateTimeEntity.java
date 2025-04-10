a7bd22519834d8e3b1084e5b7df55ddd572d7e2a
GwonDooHyeon <secure3141@gmail.com>
2025-04-10 21:50:22 +0900

    feat: implement file entity with auditing support

---
+++ src/main/java/com/helper/filekit/domain/BaseDateTimeEntity.java
@@ -0,0 +1,27 @@
+package com.helper.filekit.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseDateTimeEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/com/helper/filekit/domain/file/StorageType.java
@@ -0,0 +1,6 @@
+package com.helper.filekit.domain.file;
+
+public enum StorageType {
+    AMAZON_S3,
+    LOCAL
+}
