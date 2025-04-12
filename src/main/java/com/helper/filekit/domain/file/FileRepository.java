3d6c179539328710925a569ea787374a9c515600
GwonDooHyeon <secure3141@gmail.com>
2025-04-12 10:38:15 +0900

    chore: domain 삭제 -> 라이브러리 구현이기 때문에 불필요

--- src/main/java/com/helper/filekit/domain/file/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.helper.filekit.domain.file;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<FileEntity, Long> {
-
-}
--- src/main/java/com/helper/filekit/domain/file/StorageType.java
+++
@@ -1,6 +0,0 @@
-package com.helper.filekit.domain.file;
-
-public enum StorageType {
-    AMAZON_S3,
-    LOCAL
-}
